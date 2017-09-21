ea1bb34d723ca5145ff8b5db9b5c50fcb26080e0
unknown <emil.mametov.01@mail.ru>
2017-09-21 20:02:32 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var years = prompt('Skolko vam let', 20);
+alert('Vam ' + years + ' let!')
+
+if (expression 1){
+   Statement(s) to be executed if expression 1 is true
+}
+
+else if (expression 2){
+   Statement(s) to be executed if expression 2 is true
+}
+
+else if (expression 3){
+   Statement(s) to be executed if expression 3 is true
+}
+
+else{
+   Statement(s) to be executed if no expression is true
+}
